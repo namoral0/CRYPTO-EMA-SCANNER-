4dbb4438047fbcb3d76033d13d0aa32fc960e916
namoral0 <baromecuk@gmail.com>
2026-08-22 11:58:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 GOOGLE_TASKS_CREDENTIALS = os.getenv("GOOGLE_TASKS_CREDENTIALS")
 
 # Oficjalne symbole Kraken Pro (w tym oznaczona giełdowo para fiat)
-SYMBOLS = ["XRP/GBP", "BTC/GBP", "ETH/GBP", "LINK/GBP", "SOL/GBP", "ONDO/GBP:GBP"]
+SYMBOLS = ["XRP/GBP", "BTC/GBP", "ETH/GBP", "LINK/GBP", "SOL/GBP", "ONDO/GBP"]
 CACHE_FILE = "cache.json"
 
 def send_telegram_alert(msg):
